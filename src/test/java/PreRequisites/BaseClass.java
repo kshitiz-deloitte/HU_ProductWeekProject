de9de6552bf04605c994b61469b08efaa964f9e1
Kritik-deloitte <Kribansal@deloitte.com>
2022-04-25 01:14:10 +0530

    Organization Registration and Dashboard Comments Commit

--- src/test/java/PreRequisites/BaseClass.java
+++ src/test/java/PreRequisites/BaseClass.java
@@ -9,13 +9,14 @@ import java.io.FileInputStream;
 import java.time.Duration;
 import java.util.Properties;
 
-
+//initialize used classes and driver
 public class BaseClass {
 
     public static WebDriver driver;
     public static Properties prop;
     public static String[] role;
 
+    // load property file for data
     @BeforeTest
     public void init() throws Exception
     {
@@ -24,6 +25,7 @@ public class BaseClass {
         role = prop.getProperty("login_user_roles").split(",");
     }
 
+    // open url into chrome driver
     public void OpenDriver(String url){
         System.setProperty(prop.getProperty("browser_type"), prop.getProperty("driver_path")); // setting driver location
         driver=new ChromeDriver();          // creating driver object
@@ -31,6 +33,7 @@ public class BaseClass {
         driver.manage().window().maximize(); // maximize window
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
+    // close the driver
     @AfterTest
     public void closeDriver(){
         driver.close();
