5c1cf511980989333073fc3e8872700628d98b28
kottv-namr <kottv@deloitte.com>
2022-04-26 17:29:32 +0530

    Added the comments for each methods and classes

--- src/test/java/PageObjects/AdminDashboardPage.java
+++ src/test/java/PageObjects/AdminDashboardPage.java
@@ -2,6 +2,13 @@ package PageObjects;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
 
 public class AdminDashboardPage {
     WebDriver driver;
@@ -36,88 +43,132 @@ public class AdminDashboardPage {
     //recently added
     static By filt = By.xpath("//*[@id=\"filter\"]");
     static By compname = By.xpath("//*[@id=\"filter\"]/option[4]");
-    static By search = By.xpath("//*[@id=\\\"root\\\"]/div/div[2]/div[2]/div/div/table/tbody/tr[1]/td[3]");
+    static By companyName = By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div/table/tbody/tr[1]/td[3]");
     static By verifybtn = By.xpath("//button[@class=\"button-four\"]");
     static By lstno1 = By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div/table/tfoot/div/div/p[2]");
     static By kyc = By.xpath("//*[@id=\\\"root\\\"]/div/div[2]/div[2]/div/div/table/tbody/tr[\" + Integer.toString(row) + \"]/td[4]/p");
-    static By verify = By.xpath("//*[@id=\\\"root\\\"]/div/div[2]/div[2]/div/div/table/tbody/tr[\" + Integer.toString(row) + \"]/td[5]/button");
+    static By verify;
     static By fields = By.xpath("//*[@id=\"root\"]/div/div[2]/div/div[1]/div[1]/div/div[2]/div/div/div/div[3]/p[2]");
     static By rowpgarr = By.xpath("//button[@title=\"Next page\"]");
     static By lstno2 = By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div/table/tfoot/div/div/p[2]");
+    static By searchCompName;
+    static By rowsperpage = By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div/table/tfoot/div/div/p[2]");
     //static By beforekycstatus = By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div/table/tbody/tr[" + Integer.toString() + "]/td[4]/p");
 
+    //Getting the roes per page as a string
+    public String listNumber()
+    {
+        return driver.findElement(rowsperpage).getText();
+    }
 
+    //Before taking the action for reject/Accept the request it will return the kys status of the employee
+    public String beforeAcionKYC(int row){
+        return driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div/table/tbody/tr[" + Integer.toString(row) + "]/td[4]/p")).getText();
+    }
 
+    //After taking the action for reject/Accept the request it will return the kys status of the employee
+    public String afterActionKYC(int row){
+        return driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div/table/tbody/tr[" + Integer.toString(row) + "]/td[4]/p")).getText();
+    }
+
+    //Clicking on the filter button
+    public int filter(){
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOfElementLocated(filt));
+        List<WebElement> arrow = driver.findElements(filt);
+        System.out.println(arrow.size());
+        return arrow.size();
+    }
+
+    //Getting the string value of company name
+    public void comp(){
+        driver.findElement(compname).click();
+    }
+
+    // Getting the Searched Company name
+    public String search(){
+        return driver.findElement(companyName).getText();
+    }
+
+    // Clicking on verify button
+    public void verifyBtnWorking(){ driver.findElement(verifybtn).click(); }
+
+    // Getting the number of pages
+    public void listNumber1() {
+        String listno = driver.findElement(lstno1).getText();
+    }
+
+    //Getting the kys status
+    public String kycstat(){
+        return driver.findElement(kyc).getText();
+    }
+
+    //Clicking on verify button when the kyc status is pending
+    public void verifyy(int row){
+        verify = By.id("//*[@id=\"root\"]/div/div[2]/div[2]/div/div/table/tbody/tr["+row+"]/td[5]/button");
+        verify.findElement(driver).click();
+    }
+
+    //Employee details string value
+    public String field(){ return driver.findElement(fields).getText(); }
+    public void rowperPageArrow(){ driver.findElement(rowpgarr).click(); }
+
+    // Getting the number of pages after reject/accepting the kys status
+    public String listNumber2(){ return driver.findElement(lstno2).getText();
+    }
+
+    //Webdriver opening
     public AdminDashboardPage(WebDriver driver){
         this.driver = driver;
     }
 
+    // Clicking on Reject button
     public void clickRejectButton(){
         driver.findElement(rejectButton).click();
     }
+
+    // Clicking on Accept button
     public void clickAcceptButton(){
         driver.findElement(acceptButton).click();
     }
+
+    //Clicking on Message and writing
     public void clickMessageInput(){
         driver.findElement(messageInput).click();
     }
 
+    //clicking on Arrow Mark
     public void clickArrowMark(){
         driver.findElement(arrowButton).click();
     }
+
+    //Clicking on filter option
     public void clickFilterButton(){
         driver.findElement(fileterButton).click();
     }
+
+    //Clicking on Search button with company name
     public void clickSearchCompany(String property){
         driver.findElement(searchcompanyName).click();
     }
+    //Clicking on search button
     public void clickSearchButton(){
         driver.findElement(searchButton).click();
     }
+    //clicking on Download button of aadhar card
     public void clickAadarDownloadButton(){
         driver.findElement(adharDownloadButton).click();
     }
+
+    //Clicking on pan card
     public void clickPanDownloadButton(){
         driver.findElement(pandownloadButton).click();
     }
-    public void clickHighestEducationDegreeDownloadButton(){
-        driver.findElement(highestEducationDegreeDownloadButton).click();
-    }
-    public void clickInstructionPersonalInfo(){
-        driver.findElement(instruction_personalInformation).click();
-    }
-    public void clickInstructionContactInfo(){
-        driver.findElement(instruction_contactInformation).click();
-    }
-    public void clickInstructionEducationalInfo(){
-        driver.findElement(instruction_educationalInformation).click();
-    }
-    public void clickInstructionBankDetails(){
-        driver.findElement(instruction_bankDetails).click();
-    }
-    public void clickInstructionEmployementHistory(){
-        driver.findElement(instruction_employementHistory).click();
-    }
 
-    //recently added functions
-    public void filter(){ driver.findElement(filt).click(); }
-
-    public void comp(){ driver.findElement(compname).click(); }
-
-    public void search(){ driver.findElement(search).getText(); }
-
-    public void verifyBtnWorking(){ driver.findElement(verifybtn).click(); }
-
-    public void listNumber1(){ String listno = driver.findElement(lstno1).getText(); }
-
-    public void kycstat(){ driver.findElement(kyc).getText(); }
-
-    public void verify(){ driver.findElement(verify).click(); }
-
-    public void field(){ driver.findElement(fields).getText(); }
-
-    public void rowperPageArrow(){ driver.findElement(rowpgarr).click(); }
-
-    public void  listNumber2(){ driver.findElement(lstno2).getText(); }
-
+    //Clicking on Instructions
+    public void clickHighestEducationDegreeDownloadButton(){driver.findElement(highestEducationDegreeDownloadButton).click();}
+    public void clickInstructionPersonalInfo(){driver.findElement(instruction_personalInformation).click();}
+    public void clickInstructionContactInfo(){driver.findElement(instruction_contactInformation).click();}
+    public void clickInstructionEducationalInfo(){driver.findElement(instruction_educationalInformation).click();}
+    public void clickInstructionBankDetails(){driver.findElement(instruction_bankDetails).click();}
+    public void clickInstructionEmployementHistory(){driver.findElement(instruction_employementHistory).click();}
 }
--- src/test/java/Test/AdminTest.java
+++ src/test/java/Test/AdminTest.java
@@ -3,13 +3,17 @@ package Test;
 import PageObjects.AdminDashboardPage;
 import PageObjects.HomePage;
 import PreRequisites.BaseClass;
+import org.checkerframework.checker.units.qual.A;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import PageObjects.Loginpage;
+
+import java.util.ArrayList;
 import java.util.List;
+
 
 public class AdminTest extends BaseClass {
     AdminDashboardPage adminDash;
@@ -30,93 +34,99 @@ public class AdminTest extends BaseClass {
     @Test(priority = 1)
     public void arrowMarkVerification() throws InterruptedException {
         adminDash = new AdminDashboardPage(driver);
-//        List<WebElement> arrow = adminDash.filter();
-//        int listNumberofArrow = arrow.size();
-//        if(listNumberofArrow==0) throw new AssertionError("Arrow mark is not showing up the list of Company's Name.");
+        int kb = adminDash.filter();
+        if(kb==0) throw new AssertionError("Arrow mark is not showing up the list of Company's Name.");
     }
 
     // TC002-Positive Test Case-(Choosen company name will shows up or not)
     @Test(priority = 2)
     public void companyNameSearch() throws InterruptedException {
-        String compName = "Amazon";
-//        WebElement compname = adminDash.comp();
-//        compname.click();
-//        Thread.sleep(1000);
-//        adminDash.clickFilterButton();
-//        Thread.sleep(3000);
-//        String searchCompName = adminDash.search();
-//        Assert.assertEquals(searchCompName,compName);
+        adminDash = new AdminDashboardPage(driver);
+        Thread.sleep(3000);
+        String compName = properties.getProperty("compName");
+        adminDash.comp();
+        adminDash.clickFilterButton();
+        Thread.sleep(3000);
+        String searchCompName = adminDash.search();
+        Assert.assertEquals(searchCompName,compName);
     }
 
     // TC003-Positive Test Case(Verifying whether the verify button working fine or not)
     @Test(priority = 3)
     public void verifyButton() throws InterruptedException {
-//        WebElement verifyButtonWorking = adminDash.verifyBtnWorking();
-//        verifyButtonWorking.click();
-//        Thread.sleep(3000);
-//        String expectedURL = "https://kycportal-urtjok3rza-wl.a.run.app/admin/employeDetails";
-//        String actualURL = driver.getCurrentUrl();
-//        System.out.println("actuaal is:"+actualURL);
-//        System.out.println("expected is:"+expectedURL);
-//        Assert.assertEquals(expectedURL, actualURL);
+        adminDash = new AdminDashboardPage(driver);
+        Thread.sleep(3000);
+        adminDash.verifyBtnWorking();
+        String expectedURL = properties.getProperty("employee_detail_expURL");
+        Thread.sleep(3000);
+        String actualURL = driver.getCurrentUrl();
+        System.out.println("actuaal is:"+actualURL);
+        System.out.println("expected is:"+expectedURL);
+        Assert.assertEquals(expectedURL, actualURL);
     }
 
     // TC004-Negative Test Case(Candidate's Details are not available when the requested to Admin but when the Admin either Reject or Accept the request the Details will be shown in website)
     @Test(priority = 4)
     public void candidatesDetails() throws InterruptedException {
-//        String listNumber = adminDash.listNumber();
-//        String lstNum = listNumber.substring(listNumber.length() - 2);
-//        int NumberOfRegisteredEmployee = Integer.parseInt(lstNum);
-//        // System.out.println(NumberOfRegisteredEmployee);
-//        String kycStatus = null;
-//        for (int rowsPerPage = 1; rowsPerPage <= (NumberOfRegisteredEmployee / 10); rowsPerPage++) {
-//            for (int row = 1; row <= 10; row++) {
-//                kycStatus = adminDash.kycstat();
-//                // System.out.println(beforeActionKycStatus);
-//                if (kycStatus == "Pending") {
-//                    WebElement verify = adminDash.verify();
-//                    String fields = adminDash.field();
-//                    if (fields == null) throw new AssertionError("The Candidate's Details are not Showing UP.");
-//                }
-//            }
-//            Thread.sleep(3000);
-//            WebElement rowsPerPageArrow = adminDash.rowperPageArrow();  //finding xpth difficult
-//            rowsPerPageArrow.click(); // not clicking
-//        }
+        adminDash = new AdminDashboardPage(driver);
+        Thread.sleep(3000);
+        String listNumber = adminDash.listNumber();
+        String lstNum = listNumber.substring(listNumber.length() - 2);
+        int NumberOfRegisteredEmployee = Integer.parseInt(lstNum);
+        String kycStatus = null;
+        for (int rowsPerPage = 1; rowsPerPage <= (NumberOfRegisteredEmployee / 10); rowsPerPage++) {
+            for (int row = 1; row <= 10; row++) {
+                kycStatus = adminDash.kycstat();
+                if (kycStatus == "Pending") {
+                    adminDash.verifyy(row);
+                    String fields = adminDash.field();
+                    if (fields == null) throw new AssertionError("The Candidate's Details are not Showing UP.");
+                }
+            }
+            Thread.sleep(3000);
+            adminDash.rowperPageArrow();
+        }
     }
 
     // TC005-Negative Test Case(Without reading the instruction Admin can either reject or accept the request)
     @Test(priority = 5)
     public void acceptORrejectWithoutReadingInstruction() throws InterruptedException {
-        AdminDashboardPage adminDash = new AdminDashboardPage(driver);
+        adminDash = new AdminDashboardPage(driver);
         Thread.sleep(3000);
-//        String listNumber = adminDash.listNumber2();
-//        String lstNum = listNumber.substring(listNumber.length() - 2);
-//        System.out.println(listNumber);
-//        int NumberOfRegisteredEmployee = Integer.parseInt(lstNum);
-//        System.out.println(NumberOfRegisteredEmployee);
-//        String afterActionKycStatus = null;
-//        String beforeActionKycStatus = null;
-//        for (int rowsPerPage = 1; rowsPerPage <= (NumberOfRegisteredEmployee / 10); rowsPerPage++) {
-//            for (int row = 1; row <= 10; row++) {
-//                beforeActionKycStatus = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div/table/tbody/tr[" + Integer.toString(row) + "]/td[4]/p")).getText();
-//                // System.out.println(beforeActionKycStatus);
-//                if (beforeActionKycStatus == "Pending") {
-//                    WebElement verify = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div/table/tbody/tr[" + Integer.toString(row) + "]/td[5]/button"));
-//                    verify.click();
-//                    Thread.sleep(3000);
-//                    adminDash.clickAcceptButton();
-//                    Thread.sleep(3000);
-//                    afterActionKycStatus = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div/table/tbody/tr[" + Integer.toString(row) + "]/td[4]/p")).getText();
-//                    break;
-//                }
-//            }
-//            Thread.sleep(3000);
-//            WebElement rowsPerPageArrow = driver.findElement(By.xpath("//button[@aria-label=\"Next page\"]")); //finding xpth difficult
-//            rowsPerPageArrow.click(); // not clicking
-//        }
-//        Assert.assertEquals(beforeActionKycStatus, afterActionKycStatus);
-//    }
+        String listNumber = adminDash.listNumber2();
+        String lstNum = listNumber.substring(listNumber.length() - 2);
+        System.out.println(listNumber);
+        int NumberOfRegisteredEmployee = Integer.parseInt(lstNum);
+        System.out.println(NumberOfRegisteredEmployee);
+        String afterActionKycStatus = null;
+        String beforeActionKycStatus = null;
+        for (int rowsPerPage = 1; rowsPerPage <= (NumberOfRegisteredEmployee / 10); rowsPerPage++) {
+            for (int row = 1; row <= 10; row++) {
+                beforeActionKycStatus = adminDash.beforeAcionKYC(row);
+                if (beforeActionKycStatus == "Pending") {
+                    adminDash.verifyy(row);
+                    Thread.sleep(3000);
+                    adminDash.clickAcceptButton();
+                    Thread.sleep(3000);
+                    afterActionKycStatus = adminDash.afterActionKYC(row);
+                    break;
+                }
+            }
+            Thread.sleep(3000);
+            WebElement rowsPerPageArrow = driver.findElement(By.xpath("//button[@aria-label=\"Next page\"]")); //finding xpth difficult
+            rowsPerPageArrow.click(); // not clicking
+        }
+        Assert.assertEquals(beforeActionKycStatus, afterActionKycStatus);
+    }
 
+    // TC006-Positive Test Case(Checking whether the files can be downloaded or not)
+    @Test(priority = 6)
+    public void downloadFileCheck() throws InterruptedException {
+        adminDash = new AdminDashboardPage(driver);
+//        adminDash.verifyBtnWorking();
+        adminDash.clickAadarDownloadButton();
+        List<String> browserTabs = new ArrayList<String>(driver.getWindowHandles());
+        System.out.println(browserTabs.size());
+        if(browserTabs.size()==1) throw new AssertionError("Files are not Downloadable");
     }
 }
